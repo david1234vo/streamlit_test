20662a807a1b68349dc09bc0936e951c38fb8ede
David <davidvanoverschee2@gmail.com>
2022-11-24 17:19:42 +0100

    david

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,78 @@ seattle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/mast
 stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stocks_toy.csv')
 # replace the previous data with your own streamed data from API
 
+# import requests library
+import requests 
+import json
+# import plotting library
+import matplotlib
+import matplotlib.pyplot as plt 
+from datetime import date, datetime, timedelta
+import pprint
+
+
+wind = []
+solar = []
+combined = []
+co = []
+
+for j in range(11,12):
+    for i in range(20,24):
+        
+        month = str(j)
+        date = str(i)
+        print("retrieving: "+date+"/"+month)
+        if month == 11 and date == 17:
+            break
+        endpoint = 'https://apidatos.ree.es'
+        #real time market price
+        #get_archives = '/en/datos/mercados/precios-mercados-tiempo-real'
+        get_archives = '/en/datos/generacion/estructura-generacion'
+        headers = {'Accept': 'application/json',
+                   'Content-Type': 'application/json',
+                   'Host': 'apidatos.ree.es'}
+        params = {'start_date': '2022-'+month+'-'+date+'T00:00', 'end_date': '2022-'+month+'-'+date+'T23:00', 'time_trunc':'year'}
+
+        response = requests.get(endpoint+get_archives, headers=headers, params=params)
+        json = response.json()
+
+        total_percentage = 0
+        titles = []
+        percentages = []
+        comb = 0
+        for i in range(len(json["included"])):
+            
+            t = json["included"][i]['attributes']['title']
+            if t != 'Total generation':
+                titles.append(t)
+                p = json["included"][i]['attributes']['values'][0]['percentage']
+                percentages.append(float(p)*100)
+                #print(t,":",p)
+                total_percentage += p
+                if t == "Wind":
+                    wind.append(p)
+                    comb += float(p)
+                if t == "Solar photovoltaic":
+                    solar.append(p)
+                    comb += float(p)
+                if t == "Combined cycle":
+                    combined.append(p)
+                    comb += float(p)
+        co.append(comb)
+                
+        #print(total_percentage)
+
+import pandas as pd
+import numpy as np
+
+chart_data = pd.DataFrame(
+    np.array([wind, solar, combined, co]).transpose(),
+    columns=['wind', 'solar', 'gas', 'total'])
+
+st.line_chart(chart_data)
+
+
+"""
 ### Here starts the web app design
 # Row A
 a1, a2, a3, a4 = st.columns(4)
@@ -52,3 +124,4 @@ with c2:
         data=stocks,
         theta='q2',
         color='company')
+"""
