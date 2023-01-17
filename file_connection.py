8bddceee1708a72b81a7ffe321fc1344f779a870
David <davidvanoverschee2@gmail.com>
2023-01-17 18:54:35 +0100

    david

--- file_connection.py
+++ file_connection.py
@@ -14,6 +14,7 @@ def get_data_from_file():
     while "current" in latest_file:
         latest_file = max(list_of_files, key=os.path.getctime)
         list_of_files.pop(list_of_files.index(latest_file))"""
+    print("why is this not printing")
     print("lof1", glob.glob("/"))
     print("lof2", glob.glob("./"))
     print("lof3", glob.glob("."))
